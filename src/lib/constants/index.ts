62dce801da05ff188c32b661b634ecedf277107a
Foshati <amirrezafoshati@gmail.com>
2024-05-25 23:06:07 +0330

    add img and utilities first class

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Amazonfa'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An Amazonfa clone built with Next.js, Postgres, Shadcn'
+
+export const signInDefaultValues = {
+  email: '',
+  password: '',
+}
